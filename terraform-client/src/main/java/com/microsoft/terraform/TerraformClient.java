5307267b662b0bd8568130dc004eb19c9f6a3d7a
Nico Gau <nico.gau@jungheinrich.de>
2019-08-21 08:20:17 +0200

    Add output command

--- terraform-client/src/main/java/com/microsoft/terraform/TerraformClient.java
+++ terraform-client/src/main/java/com/microsoft/terraform/TerraformClient.java
@@ -1,19 +1,35 @@
 package com.microsoft.terraform;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class TerraformClient implements AutoCloseable {
+    private static class OutputVariable {
+        @JsonProperty("type") public String type;
+        @JsonProperty("value") public Object value;
+        @JsonProperty("sensitive") public boolean sensitive;
+    }
+
     private static final String TERRAFORM_EXE_NAME = "terraform";
-    private static final String VERSION_COMMAND = "version", INIT_COMMAND = "init", PLAN_COMMAND = "plan", APPLY_COMMAND = "apply", DESTROY_COMMAND = "destroy";
+    private static final String VERSION_COMMAND = "version", INIT_COMMAND = "init", PLAN_COMMAND = "plan", APPLY_COMMAND = "apply", DESTROY_COMMAND = "destroy", OUTPUT_COMMAND = "output";
     private static final String USER_AGENT_ENV_NAME = "AZURE_HTTP_USER_AGENT", USER_AGENT_ENV_VALUE = "Java-TerraformClient", USER_AGENT_DELIMITER = ";";
     private static final Map<String, String> NON_INTERACTIVE_COMMAND_MAP = new HashMap<>();
     static {
         NON_INTERACTIVE_COMMAND_MAP.put(APPLY_COMMAND, "-auto-approve");
         NON_INTERACTIVE_COMMAND_MAP.put(DESTROY_COMMAND, "-force");
+        NON_INTERACTIVE_COMMAND_MAP.put(OUTPUT_COMMAND, "-json");
     }
 
     private final ExecutorService executor = Executors.newWorkStealingPool();
@@ -93,6 +109,39 @@ public class TerraformClient implements AutoCloseable {
     public CompletableFuture<Boolean> destroy() throws IOException {
         this.checkRunningParameters();
         return this.run(INIT_COMMAND, DESTROY_COMMAND);
+    }
+
+    public CompletableFuture<Map<String, Object>> output() throws IOException {
+        ProcessLauncher launcher = this.getTerraformLauncher(OUTPUT_COMMAND);
+        StringBuilder output = new StringBuilder();
+        Consumer<String> outputListener = this.getOutputListener();
+        launcher.setOutputListener(m -> {
+            output.append(m);
+            if (outputListener != null) {
+                outputListener.accept(m);
+            }
+        });
+
+        return launcher.launch().thenApply((c) -> {
+            if (c != 0) {
+                return Collections.emptyMap();
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                Map<String,Object> ret = new HashMap<>();
+                Map<String,OutputVariable> rawMap = objectMapper.readValue(output.toString(), new TypeReference<Map<String,OutputVariable>>(){});
+                for (Map.Entry<String, OutputVariable> entry: rawMap.entrySet()) {
+                    ret.put(entry.getKey(), entry.getValue().value);
+                }
+                return ret;
+            } catch (IOException e) {
+                if (errorListener != null) {
+                    errorListener.accept("JSON parse error: " + e.getMessage());
+                }
+                return Collections.emptyMap();
+            }
+        });
+
     }
 
     private CompletableFuture<Boolean> run(String... commands) throws IOException {
